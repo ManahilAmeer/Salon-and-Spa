0318dc036958b2d1a7c3f7771be53a8be4dd0b1a
Manahil Ameer <manahil1054@gmail.com>
2020-12-10 22:01:05 +0500

    Interface

--- MyApp/Demo/resources/views/welcome.blade.php
+++ MyApp/Demo/resources/views/welcome.blade.php
@@ -4,10 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bel Salone</title>
 
         <!-- Fonts -->
+        <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="icon" href="{{asset('/customAuth/image/hair-salon.png')}}" type="image/gif" sizes="16x16">
 
         <!-- Styles -->
         <style>
@@ -64,22 +70,41 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+                <div class="second">
+            <nav class="navbar navbar-expand-lg navbar-light">
+  <!-- <a class="navbar-brand" href="#">
+      <img src="/customAuth/image/logo1.png" class="img-fluid" alt="Responsive image" style="width:15rem;height:10rem">
+  </a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="font-weight: 700;">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" style="font-weight: 700;">Skin</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" style="font-weight: 700;" >Bath</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#" style="font-weight: 700;">Shaving</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="{{ route('login') }} " style="font-weight: 900;">Login</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="{{ route('register') }}" style="font-weight: 900;">Register</a>
+      </li>
+    </ul>
+  </div></nav></div>
+        
+  
             <div class="content">
+
                 <div class="title m-b-md">
                     Laravel
                 </div>
