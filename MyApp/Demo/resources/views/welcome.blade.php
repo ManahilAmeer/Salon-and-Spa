9cbed4d4230f016ed32f52681d665e64ddfea5d2
Manahil Ameer <manahil1054@gmail.com>
2020-12-11 12:22:59 +0500

    UI

--- MyApp/Demo/resources/views/welcome.blade.php
+++ MyApp/Demo/resources/views/welcome.blade.php
@@ -1,77 +1,24 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Bel Salone</title>
-
-        <!-- Fonts -->
-        <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-      crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="icon" href="{{asset('/customAuth/image/hair-salon.png')}}" type="image/gif" sizes="16x16">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-                <div class="second">
-            <nav class="navbar navbar-expand-lg navbar-light">
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" href="{{asset('/customAuth/css/all.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('/customAuth/css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('/customAuth/css/templatemo-style.css')}}" />
+    <title>Bel Salone</title>
+    
+<!--
+Mini Profile Template
+https://templatemo.com/tm-530-mini-profile
+-->
+  </head>
+  <body>
+    <!-- Welcome Section -->
+                    <div class="second" style="background: transparent;">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
   <!-- <a class="navbar-brand" href="#">
       <img src="/customAuth/image/logo1.png" class="img-fluid" alt="Responsive image" style="width:15rem;height:10rem">
   </a> -->
@@ -82,17 +29,39 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item">
+        <a class="nav-link" href="#" style="font-weight: 700;font-family: RozhaOne;font-size:larger;color:darksalmon">Bel ₻ Salone</a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="#" style="font-weight: 700;">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item ">
         <a class="nav-link" href="#" style="font-weight: 700;">Skin</a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link" href="#" style="font-weight: 700;" >Bath</a>
+        <a class="nav-link" href="#" style="font-weight: 700;" >Hair</a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link" href="#" style="font-weight: 700;">Shaving</a>
+        <a class="nav-link" href="#" style="font-weight: 700;">Make Up</a>
       </li>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+
       <li class="nav-item ">
         <a class="nav-link" href="{{ route('login') }} " style="font-weight: 900;">Login</a>
       </li>
@@ -101,25 +70,261 @@
       </li>
     </ul>
   </div></nav></div>
-        
-  
-            <div class="content">
-
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <section id="tmWelcome" class="parallax-window" data-parallax="scroll" data-image-src="https://source.unsplash.com/Pe9IXUuC6QU">
+      <div class="container-fluid tm-brand-container-outer">
+        <div class="row">
+          <div class="col-12">
+            <!-- Logo Area -->
+            <!-- Black transparent bg -->
+            <div class="ml-auto mr-0 tm-bg-black-transparent text-white tm-brand-container-inner">
+              <div class="tm-brand-container text-center">
+                <h1 class="tm-brand-name" style="font-family: RozhaOne;">Bel Salone</h1>
+                <p class="tm-brand-description mb-0" style="font-family: RozhaOne;">We'll Style You'll Smile</p>
+              </div>
             </div>
+          </div>
         </div>
-    </body>
+      </div>
+
+      <div class="tm-bg-white-transparent tm-welcome-container">
+        <div class="container-fluid">
+          <div class="row h-100">
+            <!-- Welcome Text -->
+            <!-- White transparent bg -->
+            <div class="col-md-7 tm-welcome-left-col">
+              <div class="tm-welcome-left">
+                <h2 class="tm-welcome-title" style="font-family: RozhaOne;">Welcome to Bel Salone</h2>
+                <p class="pb-0" style="font-family: RozhaOne;">
+                  This is Bootstrap v4.3.1 parallax layout for you. Credit goes to <a rel="nofollow" href="https://www.pexels.com">Pexels</a> for 2 background images. In
+                  odio sapien, commodo id ullamcorper ac, dignissim at sapien.
+                  Nullam leo massa, vaius ac massa et, tincidunt imperdiet
+                turpis. </p>
+              </div>
+            </div>
+            <!-- Brown bg -->
+            <div class="col-md-5">
+              <div class="tm-welcome-right">
+                <i class="fas fa-4x fa-address-card p-3 tm-welcome-icon"></i>
+                <p class="mb-0" style="font-family: RozhaOne;">
+                  Please spread a word about templatemo website. Anyone can download free Bootstrap CSS templates.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Welcome section -->
+
+    <!-- Portfolio section -->
+    <section id="tmPortfolio">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-green">
+                Project Manager
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-green">
+                  New Company
+                  <span class="tm-title-small">(2019 January to Present)</span>
+                </h3>
+                <p class="mb-0">
+                  Nullam a tellus ultricies, ornare purus vel, porttitor massa.
+                  Aliquam facilisis purus ac eros sollicidudin, in mollis neque
+                  facilisis. Duis malesuada, mi non elementum malesuada.
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-orange">
+                Senior UX Designer
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-orange">
+                  Studio One
+                  <span class="tm-title-small">(2017 April to 2018 Dec)</span>
+                </h3>
+                <p class="mb-0">
+                  Phasellus ac nulla egestas, malesuada dolor quis, scelerisque
+                  arcu. Morbi aliquam, nunc vel blandit mattis, sapien nisl
+                  convallis sem, quis hendrerit nisl tellus in lectus. Proin at
+                  nibh bibendum, tincidunt mauris sit amet, porta risus. Integer
+                  id malesuada ligula.
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-blue">
+                Graphic Designer
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-blue">
+                  Digital Com
+                  <span class="tm-title-small">(2015 Jan to 2016 Dec)</span>
+                </h3>
+                <p class="mb-0">
+                  Etiam porta est nisl, consectetur dapibus ante faucibus id.
+                  Nunc ullamcorper a quam eget tincidunt. Proin vehicula mauris
+                  ipsum, euismod dignissim dolor convallis ac.
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-dark-gray">
+                Design School
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-dark-gray">
+                  Art Design College
+                  <span class="tm-title-small">
+                    (2012 May to 2014 Dec)
+                  </span>
+                </h3>
+                <p class="mb-0">
+                  Etiam porta est nisl, consectetur dapibus ante faucibus id.
+                  Nunc ullamcorper a quam eget tincidunt. Proin vehicula mauris
+                  ipsum, euismod dignissim dolor convallis ac.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End portfolio section -->
+
+    <!-- Contact section -->
+    
+    <div id="tmContact" class="parallax-window" data-parallax="scroll" data-image-src="https://source.unsplash.com/HEde-a_T4E8">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="tm-contact-items-container">
+              <div class="tm-contact-item">
+                <i class="fas fa-5x fa-briefcase tm-contact-item-icon"></i>
+                <p class="mb-0">
+                  Quisque venenatis viverra ultrices. Lorem ipsum dolor sit
+                  amet. Nullam sit amet tempor nisl.
+                </p>
+              </div>
+              <div class="tm-contact-item">
+                <i class="fas fa-5x fa-building tm-contact-item-icon"></i>
+                <p class="mb-0">
+                  You can absolutely feel free to use Mini Profile for your web page or any usage for your website.
+                </p>
+              </div>
+
+              <div class="tm-contact-item">
+                <i class="fas fa-5x fa-balance-scale tm-contact-item-icon"></i>
+                <p class="mb-0">
+                  You are NOT allowed to re-distribute this template in any download website. Else, it is illegal action.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <form
+              action="index.html"
+              method="POST"
+              id="tmContactForm"
+              class="tm-bg-white-transparent"
+            >
+              <div class="form-group">
+                <input
+                  type="text"
+                  id="contact_name"
+                  name="contact_name"
+                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+                  placeholder="Name"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="email"
+                  id="contact_email"
+                  name="contact_email"
+                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <textarea
+                  rows="4"
+                  id="contact_message"
+                  name="contact_message"
+                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+                  placeholder="Message"
+                  required
+                ></textarea>
+              </div>
+              <div class="text-center">
+                <button
+                  type="submit"
+                  class="btn tm-btn-submit rounded-0 text-white"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- row -->
+        <div class="row">
+          <footer class="col-12">
+            <p class="text-center tm-copyright-text">
+            Copyright 2020 Mini Profile Page 
+            
+            - Design: Template Mo</p>
+          </footer>
+        </div>
+      </div>
+      <!-- container -->
+    </div>
+    <!-- section -->
+    <script src="/customAuth/js/jquery.min.js"></script>
+    <script src="/customAuth/js/parallax.min.js"></script>
+    <script>
+      function detectMsBrowser() {
+        using_ms_browser =
+          navigator.appName == "Microsoft Internet Explorer" ||
+          (navigator.appName == "Netscape" &&
+            navigator.appVersion.indexOf("Edge") > -1) ||
+          (navigator.appName == "Netscape" &&
+            navigator.appVersion.indexOf("Trident") > -1);
+
+        if (using_ms_browser == true) {
+          alert(
+            "Please use Chrome or Firefox for the best browsing experience!"
+          );
+        }
+      }
+      function setBrandMarginTop() {
+        var bottomContainerHeight = $(".tm-welcome-container").height();
+
+        $(".tm-brand-container-outer").css({
+          "margin-top": -bottomContainerHeight + "px"
+        });
+      }
+
+      $(function() {
+        setBrandMarginTop();
+        detectMsBrowser();
+
+        // Handle window resize event
+        $(window).resize(function() {
+          setBrandMarginTop();
+        });
+      });
+    </script>
+  </body>
 </html>
